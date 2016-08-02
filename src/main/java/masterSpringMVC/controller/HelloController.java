25e3c2a7e890a2f42fe08e63ca1c473a2b4da5eb
ryanOM <omullan.ryan@gmail.com>
2016-08-02 11:15:17 -0300

    Add controller

---
+++ src/main/java/masterSpringMVC/controller/HelloController.java
@@ -0,0 +1,17 @@
+package masterSpringMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "world") String name, Model model) {
+        model.addAttribute("message", "Hello " + name + "!");
+        return "resultPage";
+    }
+}
